79b0b5434db7bac84de2ab7d9ca42e57fcb96825
Zatt Wine <tomclick.org@gmail.com>
2020-08-12 15:38:08 +0630

    finished practice one

--- script.js
+++ script.js
@@ -132,3 +132,35 @@ function autoPlay() {
 let timer = setInterval(autoPlay, 5000)
 
 window.onload = load()
+
+// Fixed header
+window.onscroll = function() {
+  const docScrollTop = document.documentElement.scrollTop
+  if (window.innerWidth > 767) {
+    console.log(docScrollTop)
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed")
+    } else {
+      document.querySelector("header").classList.remove("fixed")
+    }
+  }
+}
+
+// navbar links
+const navbar = document.querySelector(".navbar")
+a = navbar.querySelectorAll("a")
+a.forEach(function(element) {
+  element.addEventListener("click", function() {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active")
+    }
+    this.classList.add("active")
+    document.querySelector(".navbar").classList.toggle("show")
+  })
+})
+
+// ham-burger
+const hamBurger = document.querySelector(".ham-burger")
+hamBurger.addEventListener("click", function() {
+  document.querySelector(".navbar").classList.toggle("show")
+})
